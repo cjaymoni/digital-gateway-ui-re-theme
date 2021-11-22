59deb7db7769fcfd7a4caa0a4affe542607fa83f
Eugene <judecl@aiti-kace.com.gh>
2021-11-22 22:36:34 +0000

    articles detail page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { RouterOutlets } from './config/app-config';
 import { LayoutComponent } from './shared-ui-modules/layout/layout.component';
 import { TestComponentComponent } from './test/test-component/test-component.component';
+import { ArticleDetailsComponent } from './pages/articles/article-details/article-details.component';
 
 const routes: Routes = [
   {
     path: 'comments',
     component: TestComponentComponent,
     outlet: RouterOutlets.Right,
+  },
+  {
+    path: 'articles' ,
+    component: ArticleDetailsComponent,
   },
 ];
 
