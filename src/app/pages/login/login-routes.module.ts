b010268ccb3b2ade87fe8fc2aea1e7762bc87803
Eugene <judecl@aiti-kace.com.gh>
2021-11-29 12:01:01 +0000

    modify login css

--- src/app/pages/login/login-routes.module.ts
+++ src/app/pages/login/login-routes.module.ts
@@ -3,10 +3,6 @@ import { RouterOutlets, SLUG_PREFIX } from 'src/app/config/app-config';
 import { Router, RouterModule, Routes, UrlSegment } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 
-// export function slugMatcher(url: UrlSegment[]) {
-//   return url[0]?.path.startsWith(SLUG_PREFIX) ? { consumed: url } : null;
-// }
-
 const rightPanelRoutes: Routes = [
   {
     path: 'login',
@@ -14,14 +10,6 @@ const rightPanelRoutes: Routes = [
     outlet: RouterOutlets.Right,
   },
 ];
-
-// const routes: Routes = [
-//   {
-//     matcher: slugMatcher,
-//     component: LoginFormComponent,
-//     data: { fetch: true },
-//   },
-// ];
 
 @NgModule({
   imports: [RouterModule.forChild([])],
