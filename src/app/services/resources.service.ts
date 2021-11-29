115fafbfb172a720115b25aa7570891414758062
Owuraku <sethg@aiti-kace.com.gh>
2021-11-29 10:37:19 +0000

    fix add error

--- src/app/services/resources.service.ts
+++ src/app/services/resources.service.ts
@@ -20,7 +20,7 @@ export class ResourceService {
   }
 
   storeResource(toStore: any, url = this.endpoint) {
-    return this.http.post(`${url}/`, toStore).pipe(map(data => data as object));
+    return this.http.post(`${url}`, toStore).pipe(map(data => data as object));
   }
 
   updateResource(toStore: any, id: any, url = this.endpoint) {
