1dbf9b44381d45fec5416815c49acf1f84109e9a
Owuraku <sethg@aiti-kace.com.gh>
2021-12-12 23:00:01 +0000

    minimized resource card

--- src/app/pages/resource/resource-card/resource-card.component.ts
+++ src/app/pages/resource/resource-card/resource-card.component.ts
@@ -1,24 +1,27 @@
-import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { Upload } from 'src/app/models/uploads.model';
 
 @Component({
   selector: 'app-resource-card',
   templateUrl: './resource-card.component.html',
   styleUrls: ['./resource-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResourceCardComponent implements OnInit {
   @Input()
   resource!: Upload;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  downloadFile(){
+  downloadFile() {
     const file = this.resource?.resource;
-    window.open(file);
+    window.open(file, '_blank');
   }
-
 }
