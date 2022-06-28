6514bd5bcf8e9c8426d886b7fe64f62b853c425c
cjaymoni <38535933+cjaymoni@users.noreply.github.com>
2022-06-28 18:45:00 +0000

    Feature/current opp link (#110)
    * faq management
    
    * contact us validation
    
    * faq positions done
    
    * management headings fix
    
    * added delete to faqs
    
    * footer errors
    
    * route to view details

--- src/app/shared-ui-modules/opportunitis-card/opportunitis-card.component.ts
+++ src/app/shared-ui-modules/opportunitis-card/opportunitis-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NavigatorService } from 'src/app/services/navigator.service';
 import { Article } from '../../models/article.model';
 
 @Component({
@@ -8,7 +9,14 @@ import { Article } from '../../models/article.model';
 })
 export class OpportunitisCardComponent implements OnInit {
   @Input() opportunity: Article | null = null;
-  constructor() {}
+  constructor(private navigator: NavigatorService) {}
 
   ngOnInit() {}
+
+  openOpportunity() {
+    this.navigator.article.goToViewDetailsPage(
+      this.opportunity?.slug as string
+    );
+  }
 }
+
